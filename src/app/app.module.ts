6d8e1833fae0b71a7c439629db58ee80cc3f9822
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-11-07 10:32:05 -0500

    134. Rutas de nuestra aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  searchUser(userId: string): void {
+    const isValidUserId = /[0-9]+/.test(userId);
+    if (!isValidUserId) return;
+    this.router.navigate(["/usuario", userId]);
   }
 
 }
